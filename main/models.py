4c8abe38e4473306eeb0dfecf6dfc9e71ae0da74
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2024-09-11 11:45:42 +0700

    added all

--- main/models.py
+++ main/models.py
@@ -1,7 +1,8 @@
-
+import uuid
 from django.db import models
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # add this line
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
